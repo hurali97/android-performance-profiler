9e8f7c524253d59f9a23d2bccc3af34d9f9c56d7
hurali97 <hurali97@gmail.com>
2022-09-07 19:05:50 +0500

    fix: custom path for cmake and ndk

--- packages/android-performance-profiler/cpp-profiler/build_for_abi.sh
+++ packages/android-performance-profiler/cpp-profiler/build_for_abi.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 ABI=$1
+PATH_TO_CMAKE=/Users/hurali/Library/Android/sdk/cmake/3.18.1/bin/cmake
+PATH_TO_NDK=/Users/hurali/Library/Android/sdk/ndk/24.0.8215888
 
 # A better config would be to have one build folder per architecture
 rm -rf CMakeFiles CMakeCache.txt BAMPerfProfiler
-cmake \
-  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+$PATH_TO_CMAKE \
+  -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake \
   -DANDROID_PLATFORM=android-23 \
   -DANDROID_ABI=$ABI \
   -DCMAKE_ANDROID_ARCH_ABI=$ABI
-cmake --build .
+$PATH_TO_CMAKE --build .
